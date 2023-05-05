419707fad48519bb283fe9b3dabdaf7971148fa8
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-05-05 19:11:06 +0530

    projectDone

---
+++ src/main/java/com/driver/repository/HotelManagementRepository.java
@@ -0,0 +1,99 @@
+package com.driver.repository;
+
+
+import com.driver.model.Booking;
+import com.driver.model.Facility;
+import com.driver.model.Hotel;
+import com.driver.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.ArrayList;
+
+@Repository
+public class HotelManagementRepository {
+
+    HashMap<String, Hotel> hotelDB = new HashMap<>();
+    HashMap<Integer, User> userMap = new HashMap<>();
+
+    HashMap<String, Booking> bookingDB = new HashMap<>();
+
+    HashMap<Integer, Integer> userbookingDB = new HashMap<>();
+
+    public String addHotel(Hotel hotel)
+    {
+        if(hotel==null || hotel.getHotelName()==null)
+            return "FAILURE";
+
+        if(hotelDB.containsKey(hotel.getHotelName())) //checking if hotel already exist in database
+            return "HOTEL ALREADY ADDED";
+
+        hotelDB.put(hotel.getHotelName(), hotel);
+        return "HOTEL ADDED SUCCESSFULLY";
+    }
+
+    public Integer addUser(User user)
+    {
+        if(userMap.containsKey(user.getaadharCardNo())) //checking if user already exist in database
+            return 0;
+
+        userMap.put(user.getaadharCardNo(), user);
+        return user.getaadharCardNo();
+    }
+
+    public List<Hotel> getAllHotels()
+    {
+        return hotelDB.values().stream().toList();
+    }
+
+    public int bookARoom(Booking booking)
+    {
+        Hotel hotel = hotelDB.get(booking.getHotelName());
+
+        if(hotel.getAvailableRooms() < booking.getNoOfRooms()) return -1;
+
+        String bookingID = UUID.randomUUID().toString();
+
+        int amount = booking.getNoOfRooms() * hotel.getPricePerNight();
+
+        booking.setAmountToBePaid(amount);
+
+        booking.setBookingId(bookingID);
+
+        bookingDB.put(bookingID, booking);
+
+        userbookingDB.put(booking.getBookingAadharCard(), userbookingDB.getOrDefault(booking.getBookingAadharCard(), 0) + 1);
+
+        return amount;
+    }
+
+    public int getBookings(Integer aadharCard)
+    {
+        return userbookingDB.get(aadharCard);
+    }
+
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName)
+    {
+        Hotel hotel = hotelDB.get(hotelName);
+
+        List<Facility> existingfacilities = hotel.getFacilities();
+
+        for(Facility facility: newFacilities)
+        {
+          if(existingfacilities.contains(facility))
+          {
+              continue;
+          }
+
+          else
+          {
+              existingfacilities.add(facility);
+          }
+        }
+        hotel.setFacilities(existingfacilities);
+        hotelDB.put(hotelName, hotel);
+        return hotel;
+    }
+}
---
+++ src/main/java/com/driver/service/HotelManagementService.java
@@ -0,0 +1,83 @@
+package com.driver.service;
+
+
+import com.driver.model.Booking;
+import com.driver.model.Facility;
+import com.driver.model.Hotel;
+import com.driver.model.User;
+import com.driver.repository.HotelManagementRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HotelManagementService {
+
+    HotelManagementRepository hotelManagementRepository = new HotelManagementRepository();
+
+    public String addHotel(Hotel hotel)
+    {
+          return hotelManagementRepository.addHotel(hotel);
+    }
+
+    public Integer addUser(User user)
+    {
+        return hotelManagementRepository.addUser(user);
+    }
+
+    public String hotelWithMostFacilities()
+    {
+        List<Hotel> hotelList = hotelManagementRepository.getAllHotels();
+        List<String> hotelNames = new ArrayList<>(); //this will store the hotel name
+        int max = 1;
+
+        if(hotelList.isEmpty()) return "";
+
+        for(Hotel hotel : hotelList)
+        {
+            if(hotel.getFacilities().size() >= max)
+                max = hotel.getFacilities().size();
+        }
+
+        for(Hotel hotel : hotelList)
+        {
+            if(hotel.getFacilities().size()==max)
+                hotelNames.add(hotel.getHotelName());
+        }
+
+        if(hotelNames.size()==0) return "";
+
+        if(hotelNames.size()==1) return hotelNames.get(0);
+
+        for(int i = 0; i < hotelNames.size()-1; i++)
+        {
+            for(int j = i+1; j < hotelNames.size(); j++)
+            {
+                if(hotelNames.get(i).compareTo(hotelNames.get(j)) > 0)
+                {
+                    String temp = hotelNames.get(i);
+                    hotelNames.add(i, hotelNames.get(j));
+                    hotelNames.add(j, temp);
+                }
+            }
+        }
+
+        return hotelNames.get(0);
+    }
+
+    public int bookARoom(Booking booking)
+    {
+       return hotelManagementRepository.bookARoom(booking);
+    }
+
+    public int getBookings(Integer aadharCard)
+    {
+        return hotelManagementRepository.getBookings(aadharCard);
+    }
+
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName)
+    {
+        return hotelManagementRepository.updateFacilities(newFacilities, hotelName);
+    }
+}
